c8108e2f5b83cd79edeac45f091b82d427556735
jorgeJim <jorge.aj93@gmail.com>
2016-10-22 15:59:32 -0400

    Twillio is done. Took me more than an hour just because Nate made a mistake while specifying the requeststs

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ from flask import Flask, render_template, url_for, request, session, redirect
 from flask_pymongo import PyMongo
 import twilio.twiml
 from twilio.rest import TwilioRestClient
+from model.integration.twilio.TwilioController import TwilioController
 
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def Hello(methods='GET'):
-    return "Hey there"
+@app.route('/hello', methods=['POST'])
+def Hello():
+    data = TwilioController.getMessage(request)
+    print(data)
+    return data
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
 
 
 
